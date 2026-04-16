4dd3326de85d83bc1062eac4e245748cd46abdec
Платіцина Ксенія <kseniia.platitsyna@nure.ua>
2026-04-16 23:05:08 +0200

    feat: додано генерацію JS коду за допомогою ШІ

--- auth.js
+++ auth.js
@@ -1,2 +1,5 @@
 // Функція для авторизації користувача через Google OAuth2
 function authenticateWithGoogle() {
+    // Ініціалізація Google API Client Library
+    gapi.load('client', initializeGoogleAPI);
+}
